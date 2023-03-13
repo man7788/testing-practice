f980acca36f33e7c084cc6924295cdf49181473e
man7788 <jerman1841@gmail.com>
2023-03-14 03:56:17 +0800

    Add capitalize function

--- src/capitalize.js
+++ src/capitalize.js
@@ -1,11 +1,8 @@
-import './style.css';
-
 const captialize = (word) => {
   const head = word.slice(0, 1).toUpperCase();
   const body = word.slice(1);
-  console.log(head);
   const combined = head + body;
   return combined;
 };
 
-console.log(captialize('diu'));
+export default captialize;
